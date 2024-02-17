b94cf875a4f767459e752a1f898418d50521b70e
Jocelyn-Gas <jocelyn.gas@dillygence.com>
2024-02-17 14:48:00 +0100

    feat: rename to casoar

--- src/casoar/main.py
+++ src/casoar/main.py
@@ -13,30 +13,29 @@ class NoConfigFoundError(Exception):
 class TooManyConfigFilesError(Exception):
     pass
 
-
 def read_config() -> Hooks:
-    config_file = Path("./.captain-hook.yml")
+    config_file = Path("./.casoar.yml")
     pyproject_file = Path("./pyproject.toml")
     pyproject_data = tomlkit.loads(pyproject_file.read_text())
 
     if "tool" not in pyproject_data.keys():
-        pyproject_data.add("captain-hook")
+        pyproject_data.add("casoar")
 
     d = pyproject_data.get("tool")
     if d is None:
-        raise NoConfigFoundError("No configuration found for captain-hook")
+        raise NoConfigFoundError("No configuration found for casoar")
 
-    if "captain-hook" in d.keys():
+    if "casoar" in d.keys():
         if config_file.exists():
             raise TooManyConfigFilesError(
-                "Both .captain-hook.yml and pyproject.toml have configuration for captain-hook"
+                "Both .casoar.yml and pyproject.toml have configuration for casoar"
             )
-        return Hooks.model_validate(pyproject_data["tool"]["captain-hook"])
+        return Hooks.model_validate(pyproject_data["tool"]["casoar"])
 
     if config_file.exists():
         return Hooks.model_validate(yaml.safe_load(config_file.read_text()))
 
-    raise NoConfigFoundError("No configuration found for captain-hook")
+    raise NoConfigFoundError("No configuration found for casoar")
 
 
 def create_sample_config(path: Path):
